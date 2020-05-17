de0be16e35c031c3cf791842585da44fd60c5669
AanandNatarajan <aanand.n@hotfoot.co.in>
2020-05-17 21:55:16 +0530

    added oracle driver

--- Program.cs
+++ Program.cs
@@ -3,52 +3,55 @@ using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
+using Oracle.ManagedDataAccess.Client;
 using DBScanner;
 
 namespace dot101
 {
     class Program
     {
-        static string name = "Aanand";
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine($"Hello {name} !");
-            Console.WriteLine("Time is : " + DateTime.Now);
-
-            // string s1 = "test";
-            // string s2 = Console.ReadLine();
-            // Console.WriteLine(s1.Equals(s2));
-            // Console.WriteLine(ReferenceEquals(s1, s2));
-
-            Decimal d1 = new Decimal(2.0);
-            Decimal d2 = new Decimal(1.9);
-            Decimal d3 = d1 - d2;
-
-            Console.WriteLine(d3);
-
-            DbConnection conn = new MySqlConnection(@"server=localhost;userid=root;password=admin@123;");
-            
-            Scanner scanner = new Scanner(conn);
-            foreach (var db in scanner.GetDatabases())
+            string dbName = null;
+            if (args.Length > 0)
             {
-                Console.WriteLine("{0}",db);
-                foreach (var tab in scanner.GetTables(db))
+                dbName = args[0];
+            }
+
+            DbConnection conn = new MySqlConnection(@"server=localhost;userid=root;password=root;");
+            //DbConnection conn = new OracleConnection(@"data source=//localhost:1521;user id=AU_CUSTOMER_PORTAL_MSME;password=Pt#24msmE;");
+
+            Scanner scanner = new Scanner(conn);
+            if (dbName != null)
+            {
+                PrintDB(scanner, dbName);
+            }
+            else
+            {
+                foreach (var db in scanner.GetDatabases())
                 {
-                    Console.WriteLine("\t{0}", tab);
-                    foreach (var col in scanner.GetTableColumns(db, tab))
-                    {
-                        Console.WriteLine("\t\t{0}", col);
-                    }
-                    
-                    foreach (var reference in scanner.GetTableReferences(db, tab))
-                    {
-                        Console.WriteLine("\t\t\t{0}", reference);
-                    }
+                    Console.WriteLine("{0}", db);
+                    PrintDB(scanner, db);
                 }
             }
 
             conn.Close();
+        }
+
+        private static void PrintDB(Scanner scanner, string db) {
+            foreach (var tab in scanner.GetTables(db))
+                    {
+                        Console.WriteLine("\t{0}", tab);
+                        foreach (var col in scanner.GetTableColumns(db, tab))
+                        {
+                            Console.WriteLine("\t\t{0}", col);
+                        }
+
+                        foreach (var reference in scanner.GetTableReferences(db, tab))
+                        {
+                            Console.WriteLine("\t\t\t{0}", reference);
+                        }
+                    }
         }
 
     }
